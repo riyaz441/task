9d8028127a72e6e4f2e16cb21fa0bedc5ace9372
riyaz441 <riyazmohamed441@gmail.com>
2022-11-02 10:15:50 +0530

    inspect element disabled on student profile update page

--- profileupdate.php
+++ profileupdate.php
@@ -261,7 +261,7 @@ try {
     <div id="layoutSidenav_content">
         <main>
 
-            <body class="bg-light">
+            <body class="bg-light" oncontextmenu="return false;">
 
                 <div class="container">
                     <main>
@@ -492,6 +492,29 @@ $con = null;
                             }
                         });
                     });
+                </script>
+
+                <!-- block the inspect element -->
+                <script>
+                    document.onkeydown = function(e) {
+
+                        if (event.keyCode == 123) {
+                            return false;
+                        }
+
+                        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+                            return false;
+                        }
+
+                        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+                            return false;
+                        }
+
+                        if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+                            return false;
+                        }
+
+                    }
                 </script>
 
             </body>
